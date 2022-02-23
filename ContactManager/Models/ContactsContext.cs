51fdba62fa94394e73d5a4bcdb345c3937145dfa
epeedoh <epeedoh@yahoo.fr>
2022-02-23 18:54:05 +0000

    Begin

---
+++ ContactManager/Models/ContactsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class ContactsContext: DbContext
+    {
+        public ContactsContext(): base("DefaultConnection")
+        {
+
+        }
+        public DbSet<User> UserProfiles { get; set; }
+
+    }
+}
---
+++ ContactManager/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+
+    public class User
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+
+}
